62639b05d27f4d70cb8975b9222580779d262ac2
sijbaby <cydalsij@outlook.com>
2018-09-22 13:53:32 +0100

    added remove duplicates functionality

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,52 @@
+# import libraries
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 import sys
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Outputs merged and loaded csv files"""
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    #Merge datasets
+    df = pd.merge(messages, categories, on="id")
+
+    return(df)
 
 
 def clean_data(df):
-    pass
+    """Outputs cleaned df"""
+
+    #Preprocessing
+    cols = df.categories.str.split(";", expand=True).iloc[0]
+
+    # Split category and take first part as col names
+    for i in range(len(cols)):
+        cols[i] = cols[i].split("-")[0]
+
+    category = df.categories.str.split(";", expand=True)
+    category.columns = cols
+
+    # Drop first part of categories and convert to int
+    for col in cols:
+        category[col] = pd.to_numeric(category[col].str.split("-").str[1])
+
+    newdf = pd.concat([df, category], axis=1)
+    newdf = newdf.drop(["categories"], axis=1)
+
+
+
+    return(newdf)
 
 
 def save_data(df, database_filename):
-    pass  
+    """Saves cleaned data to DB"""
+
+    #Create engine and save to sql db
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('message', engine, index=False)
 
 
 def main():
@@ -24,12 +60,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
